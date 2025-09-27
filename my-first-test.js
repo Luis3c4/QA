3a2762536b2033c370f2585657c0cb632a780b6e
luis3c4 <u20231587@utp.edu.pe>
2025-09-27 13:12:02 -0500

    stages

--- my-first-test.js
+++ my-first-test.js
@@ -1,11 +1,16 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
 
 export const options = {
-    // 10 iterations
-    iterations: 10,
+    stages: [
+        { duration: '30s', target: 20 }, // ramp up to 20 users
+        { duration: '1m', target: 10 },  // stay at 20 users
+        { duration: '10s', target: 0 },  // ramp down to 0 users
+    ],
 };
 export default function (){
-    http.get('https://quickpizza.grafana.com');
+    const res = http.get('https://quickpizza.grafana.com/');
+    check(res, { 'status was 200': (r) => r.status == 200 });
+
     sleep(1);
 }
