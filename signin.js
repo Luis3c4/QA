55d5473c8b8b75dc579b234ba74c63229c2ff49d
luis3c4 <u20231587@utp.edu.pe>
2025-10-01 18:13:04 -0500

    signin

---
+++ signin.js
@@ -0,0 +1,16 @@
+import http, { head } from "k6/http";
+
+export default function () {
+  const url = "http://test.k6.io/login";
+  const payload = JSON.stringify({
+    username: "aaa",
+    password: "bbb",
+  });
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  http.post(url, payload, params);
+}
